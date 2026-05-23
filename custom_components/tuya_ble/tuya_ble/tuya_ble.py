b9df4210e7ba13ff7be0daebd5b70c3f807b40b8
cpw <cpw+github@weeksfamily.ca>
2026-05-23 15:52:43 -0400

    Log GATT packet writes

--- custom_components/tuya_ble/tuya_ble/tuya_ble.py
+++ custom_components/tuya_ble/tuya_ble/tuya_ble.py
@@ -1224,7 +1224,11 @@ class TuyaBLEDevice:
         for packet in packets:
             if self._client:
                 try:
-                    # _LOGGER.debug("%s: Sending packet: %s", self.address, packet.hex())
+                    _LOGGER.debug(
+                        "%s: Writing GATT packet: %s",
+                        self.address,
+                        packet.hex(),
+                    )
                     await self._client.write_gatt_char(
                         CHARACTERISTIC_WRITE,
                         packet,
